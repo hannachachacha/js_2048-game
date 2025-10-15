5173b5922e47edc90b801cfbe0425ee663e99527
Hanna <hannachachacha7@gmail.com>
2025-10-15 16:41:50 +0300

    adding project code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+import Game from '../modules/Game.class.js';
+
+const game = new Game();
+
+const startButton = document.querySelector('.start');
+
+startButton.addEventListener('click', () => {
+  if (!game.hasStarted) {
+    game.start();
+  } else {
+    game.restart();
+  }
+});
